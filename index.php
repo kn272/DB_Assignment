602702cc1f14d54d5ebe6ed476b4111922acc765
kishore kumar reddy nerkundram gajendran <kn272@afsconnect1.njit.edu>
2017-11-15 20:48:03 -0500

    added dbConn class

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php
 
+  ini_set('display_errors','On');
+  error_reporting(E_ALL);
+/*
+  define('DATABASE', 'kn272');
+  define('USERNAME', 'kn272');
+  define('PASSWORD', 'KYhRX7n1z');
+  define('CONNECTION', 'sql.njit.edu');
+*/
+
+  class dbConn{
+     protected static $db;
+
+     private function __construct() {
+        try {
+           //self::$db = new PDO( 'mysql:host=' . CONNECTION . ';dbname=' . DATABASE, USERNAME, PASSWORD );
+	   self::$db = new
+	   PDO("mysql:host=sql.njit.edu;dbname=kn272,kn272,KYhRX7n1z");
+	   self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	   echo 'Connected successfully<br>';
+        }
+	catch (PDOException $e) {
+           echo "Connection Error: " . $e->getMessage();
+	}
+     }
+
+     private static function getConn() {
+        if (!self::$db) {
+           new dbConn();
+	}
+
+	return self::$db;
+     }
+ 
+  }
 ?>
